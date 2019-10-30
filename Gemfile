d901f9d20a1fc00dae66abcb7c047de62c887740
BHolmes53 <bill.n.holmes@gmail.com>
2019-10-30 16:11:37 +0000

    devise update

--- Gemfile
+++ Gemfile
@@ -68,7 +68,7 @@ source 'https://rails-assets.org' do
 end
 
 gem 'simple_form'
-gem 'devise'
+gem 'devise', '>= 4.7.1'
 gem 'carrierwave', '~> 1.3.1'
 gem 'figaro'
 gem 'fog-aws'
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,20 +1,23 @@
 class LessonsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson.section.course, only [:show]
+      before_action :authenticate_user!
+      before_action :check_enrollment
+  
+
+  def show
+  end
 
   private
 
-  def require_authorized_for_current_lesson
-    if current_lesson.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
+
+  def check_enrollment
+    if current_user.enrolled_in?(current_lesson.section.course) 
+    else
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not Enrolled in this Course'
     end
-  end
-
-
+  end 
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-
 end
